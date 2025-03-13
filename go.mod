394462b82a2e193e5262f9cc8db0937073b2af61
Thomas Gerbet <thomas.gerbet@enalean.com>
2025-03-13 16:52:14 +0100

    chore: Bump nixpkgs pin

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/enalean/terraform-provider-mailjet
 
-go 1.22.2
+go 1.24.1
 
 require (
 	github.com/google/go-cmp v0.6.0
--- shell.nix
+++ shell.nix
@@ -1,13 +1,13 @@
 { pkgs ? import (
   fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/f2d7a289c5a5ece8521dd082b81ac7e4a57c2c5c.tar.gz";
-    sha256 = "sha256:1r62iaxd4hzimcpisssy3j0394iw9icc9kggdajl8rv9w2w0mpv1";
+    url = "https://github.com/NixOS/nixpkgs/archive/b62d2a95c72fb068aecd374a7262b37ed92df82b.tar.gz";
+    sha256 = "sha256:0rxdf3jhh3ac3z361gz6cjqrd3vvnd9wbsljxi6rmcgq01snmm3h";
   }
 ) {} }:
 
 pkgs.mkShellNoCC {
   buildInputs = [
-    pkgs.go_1_22
+    pkgs.go_1_24
     pkgs.gitMinimal
     pkgs.goreleaser
     pkgs.syft
